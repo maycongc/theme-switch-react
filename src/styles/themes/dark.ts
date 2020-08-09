e62504dc10f398ba19cee89a9da46d14256f2b4d
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2020-08-09 12:04:27 -0300

    chore: update colors theme

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -1,12 +1,14 @@
 export default {
   title: 'dark',
   colors: {
-    primaryDark: '#DD2D4A',
-    primary: '#AF1D35',
-    secondaryDark: '#F49CBB',
-    secondary: '#F26A8D',
-    tertiaryDark: '#E01530',
+    primary: '#DD2D4A',
+    primaryDark: '#AF1D35',
+
+    secondary: '#F49CBB',
+    secondaryDark: '#F26A8D',
+
     tertiary: '#BB1128',
+    tertiaryDark: '#E01530',
     background: '#1E1B18',
     text: '#fff',
   },
--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -1,12 +1,15 @@
 export default {
   title: 'light',
   colors: {
-    primary: '#DD2D4A',
-    primaryDark: '#AF1D35',
-    secondary: '#F49CBB',
-    secondaryDark: '#F26A8D',
+    primary: '#F49CBB',
+    primaryDark: '#F26A8D',
+
+    secondary: '#DD2D4A',
+    secondaryDark: '#AF1D35',
+
     tertiary: '#E01530',
     tertiaryDark: '#BB1128',
+
     background: '#f5f5f5',
     text: '#1E1B18',
   },
